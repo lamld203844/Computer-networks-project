b13fd6819110f6db0cc5c885feba7cfd79d89ac4
lamld203844 <78334320+lamld203844@users.noreply.github.com>
2023-05-06 21:30:55 +0700

    OOP and try with short file

--- client.py
+++ client.py
@@ -1,27 +1,35 @@
 import socket
+import os
+HEADER_SIZE = 10
 
-# Create a socket object
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+class Client:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.sequence_num = int(0)
+        
+    def send_file(self, file_path):
+        # Open a socket and connect to the server
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((self.host, self.port))
+            
+            # Send the file name and size to the server
+            file_name = os.path.basename(file_path)
+            file_size = os.path.getsize(file_path)
+            s.sendall(f"{file_name}:{file_size}".encode())
+            
+            # Send the file data in chunks with sequence numbers
+            with open(file_path, "r") as f:
+                while True:
+                    data = f.read(1024)
+                    if not data:
+                        break
+                    packet = f"{self.sequence_num:<{HEADER_SIZE}}" + data
+                    s.sendall(packet.encode())
+                    self.sequence_num += 1
 
-# Connect to the server on a specific IP address and port
-flag = client_socket.connect(('localhost', 12345))
+host = socket.gethostname() 
+ip_address = socket.gethostbyname(host)
 
-# Connect successfully
-if flag == None:
-    # init full data
-    full_data = ''
-
-    # receive stream
-    while True:
-        # Receive packet from the server 8 bytes at a time
-        data = client_socket.recv(8)
-        if len(data) <= 0:
-            break
-        full_data += data.decode('utf-8')
-    if len(full_data) > 0:
-        print(f'Received data: {full_data}')
-else:
-    print('Error connection')
-
-# Close the client socket
-client_socket.close()
+client = Client(ip_address, 1234)
+client.send_file(os.getcwd()+'\hello.pdf')
--- server.py
+++ server.py
@@ -1,26 +1,46 @@
 import socket
+import os
 
-# create the socket
-# AF_INET == ipv4
-# SOCK_STREAM == TCP
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+HEADER_SIZE = 10
 
-# Bind the socket to a specific IP address and port
-server_socket.bind(('localhost', 12345))
+class Server:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        
+    def receive_file(self):
+        # Open a socket and listen for connections
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((self.host, self.port))
+            s.listen()
+            conn, addr = s.accept()
+            
+            # Receive the file name and size from the client
+            file_info = conn.recv(1024).decode()
+            file_name, file_size = file_info.split(":")
+            file_size = int(file_size)
+            print(f'{file_name}, {file_size}')
+            
+            # Receive the file data in chunks with sequence numbers
+            received_data = ""
+            expected_sequence_num = 0
+            while len(received_data) < file_size:
+                data = conn.recv(1024).decode()
+                if not data:
+                    break
+                sequence_num = int(data[:HEADER_SIZE])
+                chunk = data[HEADER_SIZE:]
+                if sequence_num == expected_sequence_num:
+                    received_data += chunk
+                    expected_sequence_num += 1
+                else:
+                    print(f"Packet loss detected: expected {expected_sequence_num}, got {sequence_num}")
+            
+            # Save the received file data to disk
+            print('Received successfully')
 
-### incoming = listen for incoming connection (5 queues)
-server_socket.listen(5)
+host = socket.gethostname() 
+ip_address = socket.gethostbyname(host)
 
-print('Server listening for incoming connections...')
-
-while True:
-    # Accept a client connection
-    client_socket, client_address = server_socket.accept()
-
-    print(f'Connection from: {client_address}')
-
-    # Send a message to the client
-    client_socket.send(b'Hello from the server!')
-
-    # Close the client socket
-    client_socket.close()
+server = Server(ip_address, 1234)
+server.receive_file()
